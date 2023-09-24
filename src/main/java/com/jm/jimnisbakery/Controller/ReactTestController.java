286d89423e97689823d8fe812a2482100dfe5c5c
taehun2767 <taehun.lee@zep.us>
2023-09-24 15:57:25 +0900

    feat : setting react environment

---
+++ src/main/java/com/jm/jimnisbakery/Controller/ReactTestController.java
@@ -0,0 +1,13 @@
+package com.jm.jimnisbakery.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReactTestController {
+
+    @GetMapping("/api/hello")
+    public String test() {
+        return "Hello, world!";
+    }
+}
