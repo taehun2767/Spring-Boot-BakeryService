b084d66ab67dada08103f527db778fece2a36d8b
taehun2767 <taehun.lee@zep.us>
2023-10-13 00:23:34 +0900

    feat : add clss for API route

--- src/main/java/com/jm/jimnisbakery/common/JRoute.java
+++ src/main/java/com/jm/jimnisbakery/common/JRoute.java
@@ -1,4 +1,11 @@
 package com.jm.jimnisbakery.common;
 
+
+import lombok.Getter;
+
+
 public class JRoute {
+    //bread
+    public static final String API_BREADS = "/api/breads";
+    public static final String API_BREAD_DETAILS = "/api/bread/details";
 }
--- src/main/java/com/jm/jimnisbakery/web/controller/BreadController.java
+++ src/main/java/com/jm/jimnisbakery/web/controller/BreadController.java
@@ -1,8 +1,15 @@
 package com.jm.jimnisbakery.web.controller;
 
+import com.jm.jimnisbakery.common.ApiResult;
+import com.jm.jimnisbakery.common.JRoute;
+import com.jm.jimnisbakery.web.dto.BreadDto;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BreadController {
+    @GetMapping(value = JRoute.API_BREADS)
+    public ApiResult<BreadDto> GetBreadDetails(){
 
+    }
 }
